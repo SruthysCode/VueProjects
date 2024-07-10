3729e078611cabb8434d604671ca10eda9162e4a
Sruthy <sruthyrobin@gmail.com>
2024-07-10 22:34:24 +0530

    weatherapp with tailwind

---
+++ WeatherTailwind/weathertailwindApp/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+import SearchInput from './components/SearchInput.vue'
+
+import WeatherCard from './components/WeatherCard.vue'
+import './style.css'
+
+const places = ref([])
+
+const addPlace = (data) => {
+  places.value.push(data)
+}
+
+const deletePlace = (name) => {
+  if (confirm('Are you sure')) {
+    places.value = places.value.filter((p) => p.location.name !== name)
+  }
+}
+</script>
+<template>
+   <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-800">Hello, Tailwind CSS with Vue!</h1>
+  </div>
+  <main>
+    <!-- Date -->
+    <div class="text-center mb-6">
+      {{
+        new Date().toLocaleDateString('en-us', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        })
+      }}
+    </div>
+
+    <!-- Search -->
+    <div>
+      <SearchInput @place-data="addPlace" />
+    </div>
+
+    <!-- Weather cards -->
+    <div class="grid grid-cols-2 gap-4">
+      <div v-for="(place, idx) in places" :key="idx">
+        <WeatherCard :place="place" @delete-place="deletePlace" />
+      </div>
+    </div>
+  </main>
+</template>
